f2711c3462e88b24903fe430b848bbb16fde1229
Omkar Chandorkar <gotenksIN@aospa.co>
2026-08-31 05:19:13 +0000

    wifi_hal: Fix build with updated wpa_supplicant
    See 61966ace9b42 in external/wpa_supplicant_8.
    
    Change-Id: I35aa56ce851ccb38fab56d206a5fd821cb624b5f
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- qcwcn/wifi_hal/nan_pairing_initiator.cpp
+++ qcwcn/wifi_hal/nan_pairing_initiator.cpp
@@ -64,7 +64,7 @@ int nan_pairing_initiator_pmksa_cache_add(struct rsn_pmksa_cache *pmksa,
                                           u32 pmk_len)
 {
     if (pmksa_cache_add(pmksa, pmk, pmk_len, NULL, NULL, 0, bssid, own_addr,
-                        NULL, WPA_KEY_MGMT_SAE, 0))
+                        NULL, WPA_KEY_MGMT_SAE, NULL, 0))
           return 0;
     return -1;
 }
@@ -84,7 +84,7 @@ int nan_pairing_initiator_pmksa_cache_get(struct rsn_pmksa_cache *pmksa,
 
 void nan_pairing_initiator_pmksa_cache_flush(struct rsn_pmksa_cache *pmksa)
 {
-    return pmksa_cache_flush(pmksa, NULL, NULL, 0, false);
+    return pmksa_cache_flush(pmksa, NULL, NULL, 0, false, NULL);
 }
 
 void NanCommand::notifyPairingInitiatorResponse(transaction_id id, u32 pairing_id)
